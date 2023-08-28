4908601dc24b3d050be3d5ca4cb38c7c7277ab04
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-28 21:03:58 +0700

    Mencoba memperbaiki error ENOENT

--- src/middleware/upload.image.js
+++ src/middleware/upload.image.js
@@ -10,7 +10,7 @@ const multerUpload = multer({
       try {
         // tempat menyimpan file image
         if (file.fieldname === "image") {
-          cb(null, "./public/image");
+          cb(null, "./image");
         }
         // file jenis lain akan ditolak
         else {
