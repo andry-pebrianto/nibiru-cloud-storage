9738f68e1af58bfa73ecb0f26519400a26cc86f3
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-29 17:14:45 +0700

    Mendapatkan google drive credentials

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,4 +6,9 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   // aws
   AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+  // gcp
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  REDIRECT_URI: process.env.REDIRECT_URI,
+  DRIVE_REFRESH_TOKEN: process.env.DRIVE_REFRESH_TOKEN,
 };
